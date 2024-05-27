cec3bad979f85b969748c1a7d70426829964a583
Arun Kumar <arun.kumar.2403gg@gmail.com>
2024-05-27 08:53:58 +0530

    Added necessary templates

--- src/config/urls.py
+++ src/config/urls.py
@@ -15,8 +15,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+from accounts.views import InternalServerErrorView, PageNotFoundView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('500/', view=InternalServerErrorView.as_view(), name='internal-server-error'),
+    path('404/', view=PageNotFoundView.as_view(), name='page-not-found'),
+    path('accounts/', include('accounts.urls')),
+    path('', include('main.urls')),
+    path('subscriptions/', include('subscriptions.urls')),
+    path('orders/', include('orders.urls')),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
